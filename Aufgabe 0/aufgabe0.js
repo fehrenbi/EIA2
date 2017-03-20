c96a128e9b1a9ddd4bad6deb6e64f2c5a93b89b5
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-03-20 10:48:37 +0100

    Aufgabe 0

--- Aufgabe 0/aufgabe0.js
+++ Aufgabe 0/aufgabe0.js
@@ -2,14 +2,13 @@
 Aufgabe 0
 Name: Luise Fehrenbach
 Matrikel: 254668
-Datum: 20.03.2017   
+Datum: 20.03.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-
-
-document.addEventListener("DOMContentLoaded", function(): void {
-    var name: string;
-    name = prompt ("Gebe hier bitte deinen Namen ein");
-    var greet: string = "Guten Tag";
+document.addEventListener("DOMContentLoaded", function () {
+    var name;
+    name = prompt("Gebe hier bitte deinen Namen ein");
+    var greet = "Guten Tag";
     document.body.innerHTML = greet + " " + name;
-    });
+});
+//# sourceMappingURL=aufgabe0.js.map
