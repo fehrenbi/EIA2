c63aebf31117a9aeac29cd0d8809beead42721ff
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-03-20 10:35:32 +0100

    typescript

---
+++ Aufgabe 0/aufgabe0.ts
@@ -0,0 +1,8 @@
+/*
+Aufgabe 0
+Name: Luise Fehrenbach
+Matrikel: 254668
+Datum: 20.03.2017   
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
