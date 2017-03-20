3132b17b7709f982ca45056c08493f5e079520e8
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-03-20 10:34:20 +0100

    delete

--- Aufgabe 0/aufgabe0.js
+++
@@ -1,8 +0,0 @@
-/*
-Aufgabe 0
-Name: Luise Fehrenbach
-Matrikel: 254668
-Datum: 20.03.2017	
-Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
-*/
-
