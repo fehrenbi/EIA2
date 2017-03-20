7db2940a7c709a8f7c11278929d45a9933c19644
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-03-20 10:37:39 +0100

    speichern

--- Aufgabe 0/aufgabe0.ts
+++ Aufgabe 0/aufgabe0.ts
@@ -6,3 +6,10 @@ Datum: 20.03.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var name:string;
+    name = prompt ("Gebe hier bitte deinen Namen ein");
+    var greet:string = "Guten Tag";
+    document.body.innerHTML = greet + " " + name;
+    });
