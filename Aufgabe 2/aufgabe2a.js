70ede78fc9387e80d5af94930de80203f66c9002
luisefehrenbach <luisefehrenbach@luises-mbp.fritz.box>
2017-03-30 20:30:01 +0200

    Aufgabe 2a

--- Aufgabe 2/aufgabe2a.js
+++ Aufgabe 2/aufgabe2a.js
@@ -5,4 +5,33 @@ Matrikel: 254668
 Datum: 30.03.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
+/*let i: number = 0;
+let n: number = 64;
+*/
+let div = document.createElement("div");
+document.body.appendChild(div);
+window.onload = function () {
+    placeDivs();
+    /*let table: HTMLTableElement = document.createElement("table");
+    let row: HTMLTableRowElement = document.createElement("tr");
+    let column: HTMLTableCellElement = document.createElement("td");
+    table.appendChild(row);
+    row.appendChild(column);
+    document.body.appendChild(table);*/
+};
+function placeDivs() {
+    let rice = 1;
+    for (let i = 0; i < 64; i++) {
+        let element = document.createElement("div");
+        if (i % 2 == 0) {
+            element.className = "board white";
+        }
+        else {
+            element.className = "board black";
+        }
+        element.innerText = "" + rice;
+        rice *= 2;
+        document.body.appendChild(element);
+    }
+}
 //# sourceMappingURL=aufgabe2a.js.map
--- Aufgabe 2/aufgabe2a.ts
+++ Aufgabe 2/aufgabe2a.ts
@@ -6,3 +6,36 @@ Datum: 30.03.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 
+/*let i: number = 0;
+let n: number = 64;
+*/
+
+let div: HTMLDivElement = document.createElement("div");
+document.body.appendChild(div);
+
+window.onload = function (): void {
+    placeDivs();
+    /*let table: HTMLTableElement = document.createElement("table");
+    let row: HTMLTableRowElement = document.createElement("tr");
+    let column: HTMLTableCellElement = document.createElement("td");
+    table.appendChild(row);
+    row.appendChild(column);
+    document.body.appendChild(table);*/
+};
+
+function placeDivs(): void {
+    let rice: number = 1;
+    for (let i: number = 0; i < 64; i++) {
+        let element: HTMLElement = document.createElement("div");
+
+        if (i % 2 == 0) {
+            element.className = "board white";
+        } else {
+            element.className = "board black";
+        }
+
+        element.innerText = "" + rice;
+        rice *= 2;
+        document.body.appendChild(element);
+    }
+}
