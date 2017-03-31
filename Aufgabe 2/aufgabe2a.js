a972fc0794e53f1a64c8a4bc0594b4f1791ec16e
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-03-31 13:41:10 +0200

    Aufgabe 2a

--- Aufgabe 2/aufgabe2a.js
+++
@@ -1,8 +0,0 @@
-/*
-Aufgabe 2a
-Name: Luise Fehrenbach
-Matrikel: 254668
-Datum: 30.03.2017
-Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
-*/
-//# sourceMappingURL=aufgabe2a.js.map
--- Aufgabe 2/aufgabe2a.ts
+++
@@ -1,7 +0,0 @@
-/*
-Aufgabe 2a
-Name: Luise Fehrenbach
-Matrikel: 254668
-Datum: 30.03.2017
-Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
-*/
