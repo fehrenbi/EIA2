0cb25e7d8ae93da01ca5bee13b9debbd1e1188f9
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-04-06 12:15:33 +0200

    maumau

--- Aufgabe 3/maumau.js
+++ Aufgabe 3/maumau.js
@@ -5,5 +5,9 @@ Matrikel: 254668
 Datum: 6.4.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
-*/ 
+*/
+let karten = ['Karo7', 'Karo8', 'Karo9', 'Karo10', 'KaroBube', 'KaroDame', 'KaroKönig', 'KaroAss',
+    'Herz7', 'Herz8', 'Herz9', 'Herz10', 'HerzBube', 'HerzDame', 'HerzKönig', 'HerzAss',
+    'Pik7', 'Pik8', 'Pik9', 'Pik10', 'PikBube', 'PikDame', 'PikKönig', 'PikAss',
+    'Kreuz7', 'Kreuz8', 'Kreuz9', 'Kreuz10', 'KreuzBube', 'KreuzDame', 'KreuzKönig', 'KreuzAss'];
 //# sourceMappingURL=maumau.js.map
