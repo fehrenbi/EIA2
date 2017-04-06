eff0421468782631777c3079dab32765163082fd
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-04-06 12:29:36 +0200

    maumau

--- Aufgabe 3/maumau.ts
+++ Aufgabe 3/maumau.ts
@@ -7,7 +7,8 @@ Datum: 6.4.17
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 
-let karten: string[] = ['Karo7', 'Karo8', 'Karo9', 'Karo10', 'KaroBube', 'KaroDame', 'KaroKönig', 'KaroAss',
-                        'Herz7', 'Herz8', 'Herz9', 'Herz10', 'HerzBube', 'HerzDame', 'HerzKönig', 'HerzAss',
-                        'Pik7', 'Pik8', 'Pik9', 'Pik10', 'PikBube', 'PikDame', 'PikKönig', 'PikAss',
-                        'Kreuz7', 'Kreuz8', 'Kreuz9', 'Kreuz10', 'KreuzBube', 'KreuzDame', 'KreuzKönig', 'KreuzAss'];
+let karten: string[] = ["Karo7", "Karo8", "Karo9", "Karo10", "KaroBube", "KaroDame", "KaroKönig", "KaroAss",
+                        "Herz7", "Herz8", "Herz9", "Herz10", "HerzBube", "HerzDame", "HerzKönig", "HerzAss",
+                        "Pik7", "Pik8", "Pik9", "Pik10", "PikBube", "PikDame", "PikKönig", "PikAss",
+                        "Kreuz7", "Kreuz8", "Kreuz9", "Kreuz10", "KreuzBube", "KreuzDame", "KreuzKönig", "KreuzAss"];
+
