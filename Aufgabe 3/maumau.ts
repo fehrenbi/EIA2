28ce5b93730243d5e65b9dfdac94f02f27ccfd59
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-04-09 15:44:45 +0200

    maumau

--- Aufgabe 3/maumau.ts
+++ Aufgabe 3/maumau.ts
@@ -29,8 +29,8 @@ document.addEventListener('DOMContentLoaded', function () {
         "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Ass", "Herz Bube", "Herz Dame",
         "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Ass", "Pik Bube", "Pik Dame",
         "Kreuz 7", "Kreuz 8", "Kreuz 9", "Karo 10", "Kreuz Ass", "Kreuz Bube", "Kreuz Dame"];
-    let deck = document.getElementById("deck");
-    let discard = document.getElementById("discard");
+    let deck = document.getElementById("ziehstapel");
+    let discard = document.getElementById("ablegestapel");
     let hand = document.getElementById("hand");
     let handCards = []; //Array mit 5 gezogenen Karten 
     let i = 0;
