23def752738a203714198e3d67c94b5b1deffb82
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-04-09 17:07:58 +0200

    maumau

--- Aufgabe 3/maumau.ts
+++ Aufgabe 3/maumau.ts
@@ -7,33 +7,19 @@ Datum: 6.4.17
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 
-/*let karten: string[] = ["Karo7", "Karo8", "Karo9", "Karo10", "KaroBube", "KaroDame", "KaroKönig", "KaroAss",
-                        "Herz7", "Herz8", "Herz9", "Herz10", "HerzBube", "HerzDame", "HerzKönig", "HerzAss",
-                        "Pik7", "Pik8", "Pik9", "Pik10", "PikBube", "PikDame", "PikKönig", "PikAss",
-                        "Kreuz7", "Kreuz8", "Kreuz9", "Kreuz10", "KreuzBube", "KreuzDame", "KreuzKönig", "KreuzAss"];
-let kartenAufHand: number = 0;
-let abgelegteKarten: number = 0;
-let restkarten: number = karten.length - KartenAufHand;
-
-document.getElementById("ziehstapel").addEventListener("click", function() {
-    if (kartenAufHand < 5 && restkarten > 0) {
-        let zufälligeKarte: number = Math.floor((Math.random() * 31) + 0); 
-        while (karten[zufälligeKarte]) == undefined) {
-            zufälligeKarte = Math.floor((Math.random() * 31) + 0);
-    }
-    
-*/
     
 document.addEventListener('DOMContentLoaded', function () {
-    let allCards = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Ass", "Karo Bube", "Karo Dame",
+    
+    let allCards= ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Ass", "Karo Bube", "Karo Dame",
         "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Ass", "Herz Bube", "Herz Dame",
         "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Ass", "Pik Bube", "Pik Dame",
         "Kreuz 7", "Kreuz 8", "Kreuz 9", "Karo 10", "Kreuz Ass", "Kreuz Bube", "Kreuz Dame"];
-    let deck = document.getElementById("deck");
-    let discard = document.getElementById("discard");
-    let hand = document.getElementById("hand");
-    let handCards = []; //Array mit 5 gezogenen Karten 
+    let deck= document.getElementById("deck");
+    let discard= document.getElementById("discard");
+    let hand= document.getElementById("hand");
+    let handCards= []; 
     let i = 0;
+    
     deck.addEventListener("click", take);
     function take(_event) {
         if (i < 5) {
