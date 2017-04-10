81be40f0561fd592ff2e531e69c88500cfd006a1
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-04-10 11:52:00 +0200

    3b

--- Aufgabe 3/3b/aufgabe3b.js
+++ Aufgabe 3/3b/aufgabe3b.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     //Array anlegen
-    let kartenSammlung = ['Pik 7', 'Pik 8', 'Pik 9',
+    /*let kartenSammlung: string[] = ['Pik 7', 'Pik 8', 'Pik 9',
         'Pik 10', 'Pik Bube', 'Pik Dame',
         'Pik König', 'Pik As',
         'Kreuz 7', 'Kreuz 8', 'Kreuz 9',
@@ -11,13 +11,17 @@ document.addEventListener("DOMContentLoaded", function () {
         'Karo König', 'Karo As',
         'Herz 7', 'Herz 8', 'Herz 9',
         'Herz 10', 'Herz Bube', 'Herz Dame',
-        'Herz König', 'Herz As'];
+        'Herz König', 'Herz As'];*/
+    let allCards = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Ass", "Karo Bube", "Karo Dame",
+        "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Ass", "Herz Bube", "Herz Dame",
+        "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Ass", "Pik Bube", "Pik Dame",
+        "Kreuz 7", "Kreuz 8", "Kreuz 9", "Karo 10", "Kreuz Ass", "Kreuz Bube", "Kreuz Dame"];
     // Nachziehstapel (deck) 
     let nachziehStapel = document.getElementById("deck");
     //Eventlistener auf den Nachziehstapel
     nachziehStapel.addEventListener("click", function () {
         //Zufallskarte generieren
-        let zufallsKarte = kartenSammlung.splice(Math.round(Math.random() * (kartenSammlung.length - 1)), 1)[0];
+        let zufallsKarte = allCards.splice(Math.round(Math.random() * (allCards.length - 1)), 1)[0];
         // Variable für funktion anlegen
         let leerKarte = sucheLeerKarte();
         function sucheLeerKarte() {
@@ -31,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (leerKarte != null) {
             leerKarte.innerText = zufallsKarte;
         }
-        if (kartenSammlung.length === 0) {
+        if (allCards.length === 0) {
             nachziehStapel.innerText = "leer";
             nachziehStapel.style.display = "none";
         }
--- Aufgabe 3/3b/aufgabe3b.ts
+++ Aufgabe 3/3b/aufgabe3b.ts
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function(): void {
     //Array anlegen
-    let kartenSammlung: string[] = ['Pik 7', 'Pik 8', 'Pik 9',
+    /*let kartenSammlung: string[] = ['Pik 7', 'Pik 8', 'Pik 9',
         'Pik 10', 'Pik Bube', 'Pik Dame',
         'Pik König', 'Pik As',
         'Kreuz 7', 'Kreuz 8', 'Kreuz 9',
@@ -12,15 +12,19 @@ document.addEventListener("DOMContentLoaded", function(): void {
         'Karo König', 'Karo As',
         'Herz 7', 'Herz 8', 'Herz 9',
         'Herz 10', 'Herz Bube', 'Herz Dame',
-        'Herz König', 'Herz As'];
+        'Herz König', 'Herz As'];*/
 
+     let allCards: string [] = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Ass", "Karo Bube", "Karo Dame",
+        "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Ass", "Herz Bube", "Herz Dame",
+        "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Ass", "Pik Bube", "Pik Dame",
+        "Kreuz 7", "Kreuz 8", "Kreuz 9", "Karo 10", "Kreuz Ass", "Kreuz Bube", "Kreuz Dame"];
     // Nachziehstapel (deck) 
     let nachziehStapel: HTMLElement = document.getElementById("deck");
 
     //Eventlistener auf den Nachziehstapel
     nachziehStapel.addEventListener("click", function(): void {
         //Zufallskarte generieren
-        let zufallsKarte: string = kartenSammlung.splice(Math.round(Math.random() * (kartenSammlung.length - 1)), 1)[0];
+        let zufallsKarte: string = allCards.splice(Math.round(Math.random() * (allCards.length - 1)), 1)[0];
 
         // Variable für funktion anlegen
         let leerKarte: HTMLDivElement = sucheLeerKarte();
@@ -38,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function(): void {
             leerKarte.innerText = zufallsKarte;
         }
 
-        if (kartenSammlung.length === 0) {
+        if (allCards.length === 0) {
             nachziehStapel.innerText = "leer";
             nachziehStapel.style.display = "none"; 
         }
