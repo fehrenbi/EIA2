a14db2ac87ee342ad7b8396bc9c61ffbf5e10dca
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-04-10 10:48:43 +0200

    in js

--- Aufgabe 3/3a/Aufgabe02.js
+++ Aufgabe 3/3a/Aufgabe02.js
@@ -6,14 +6,13 @@ Datum: 31.03.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-let rice = 1;
-window.onload = function () {
-    placeDivs();
-};
-function placeDivs() {
-    // var rice: number = 1;
-    var zeile = 0;
-    for (var i = 0; i < 64; i++) {
+
+
+let sum = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    var rice: number = 1;
+    var zeile: number = 0;
+    for (var i: number = 0; i < 64; i++) {
         var element = document.createElement("div");
         if (((i + zeile) % 2) == 0) {
             element.className = "board white";
@@ -27,6 +26,46 @@ function placeDivs() {
         element.innerText = "" + rice;
         rice *= 2;
         document.body.appendChild(element);
+  
+        if (i < 8) {
+            element.addEventListener("click", mark);
+            }
+        }
+    
+    let mouseBox = document.createElement("div");
+    document.body.appendChild(mouseBox);
+    mouseBox.className = "mouseBox2";
+    document.addEventListener("mousemove", moveBox);
+    function moveBox(Event) {
+        mouseBox.style.left = (Event.clientX + 30) + "px";
+        mouseBox.style.top = (Event.clientY + 30) + "px";
     }
-}
+    
+    function mark(event) {
+        let target = event.target;
+        if (target.className.includes("marked")){
+            if (target.className.includes("black")){
+                target.className = "board black";
+            }
+            else {
+                target.className = "board white";
+            }
+        else {
+                target.className += " marked";
+                }
+        sum = 0;
+        let marked = document.getElementsByClassName("marked");
+        for (let n = 0; n < marked.length; n++) {
+            sum += parseInt(marked[n].innerText);
+            mouseBox.innerText = "Dez.:" + sum.toString() + "\r\n" + "hexDez.:" + sum.toString(16);
+            }
+            if (marked.length == 0) {
+                mouseBox.style.display = "none";
+                }
+            else {
+                mouseBox.style.display = "inline-block";
+                }
+            console.log(sum + "\r\n" + sum.toString(16));
+            }
+});
 //# sourceMappingURL=Aufgabe02.js.map
