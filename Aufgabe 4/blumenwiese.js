1b2a738663445cd133329b2663d313735f3d6a5e
luisefehrenbach <luisefehrenbach@luises-mbp.fritz.box>
2017-04-13 11:50:20 +0200

    basics ts

--- Aufgabe 4/blumenwiese.js
+++ Aufgabe 4/blumenwiese.js
@@ -1,1 +1,16 @@
+/*
+Aufgabe 4
+Name: Luise Fehrenbach
+Matrikel: 254668
+Datum: 13.4.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+document.addEventListener("DOMContentLoaded", function () {
+    let canvas;
+    canvas = document.getElementsByTagName("canvas")[0];
+    console.log(canvas);
+    let crc2;
+    crc2 = canvas.getContext("2d");
+});
 //# sourceMappingURL=blumenwiese.js.map
---
+++ Aufgabe 4/blumenwiese.ts
@@ -0,0 +1,24 @@
+/*
+Aufgabe 4
+Name: Luise Fehrenbach
+Matrikel: 254668
+Datum: 13.4.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+document.addEventListener("DOMContentLoaded", function(): void {
+    
+    let canvas: HTMLCanvasElement;
+    canvas = document.getElementsByTagName("canvas")[0];
+    console.log(canvas);
+    
+    let crc2: CanvasRenderingContext2D;
+    crc2 = canvas.getContext("2d");
+    
+    
+    
+    
+    
+    
+});
