70c7a2595026c585b2c575c0ed21e7bd196f1769
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-04-26 20:50:33 +0200

    Aufgabe 3a neu

--- Aufgabe 3/3a/3a new/a3a-new.js
+++ Aufgabe 3/3a/3a new/a3a-new.js
@@ -2,23 +2,36 @@
 Aufgabe 3a
 Name: Luise Fehrenbach
 Matrikel: 254668
-Datum: 24.04.17
+Datum: 27.04.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-var aufgabe3a;
-(function (aufgabe3a) {
-    document.addEventListener("DOMContentLoaded", function () {
-        let reis = 1;
-        function placeDivs() {
-            var zeile = 0;
-            for (var i = 0; i < 64; i++) {
-                var element = document.createElement("div");
-                if (((i + zeile) % 2) == 0) {
-                    element.className = "board white";
-                }
-            }
+window.onload = function () {
+    placeDivs();
+    /*let table: HTMLTableElement = document.createElement("table");
+    let row: HTMLTableRowElement = document.createElement("tr");
+    let column: HTMLTableCellElement = document.createElement("td");
+    table.appendChild(row);
+    row.appendChild(column);
+    document.body.appendChild(table);*/
+};
+function placeDivs() {
+    var rice = 1;
+    var rowCounter = 0;
+    for (var i = 0; i < 64; i++) {
+        var element = document.createElement("div");
+        if (((i + rowCounter) % 2) == 0) {
+            element.className = "board white";
         }
-    });
-})(aufgabe3a || (aufgabe3a = {}));
+        else {
+            element.className = "board black";
+        }
+        if (((i + 1) % 8) == 0) {
+            rowCounter++;
+        }
+        element.innerText = "" + rice;
+        rice *= 2;
+        document.body.appendChild(element);
+    }
+}
 //# sourceMappingURL=a3a-new.js.map
--- Aufgabe 3/3a/3a new/a3a-new.ts
+++ Aufgabe 3/3a/3a new/a3a-new.ts
@@ -2,33 +2,64 @@
 Aufgabe 3a
 Name: Luise Fehrenbach
 Matrikel: 254668
-Datum: 24.04.17
+Datum: 27.04.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 
-namespace aufgabe3a {
+namespace chessboard {
     
-    document.addEventListener("DOMContentLoaded", function (): void {
-        
-        let reis: number = 1;
-        
-        function placeDivs(): void {
-            var zeile: number = 0;
-            for (var i: number = 0; i < 64; i++) {
-                var element = document.createElement("div");
-                if (((i + zeile) % 2) == 0) {
-                    element.className = "boardwhite";
-                    }
-                else {
-                    element.className = "boardblack";
-                    }
-                if (((i + zeile) % 8) == 0) {
-                    zeile++;
-                }
-                element.innerText = "" + reis;
-                reis *= 2;
-                document.body.appendChild(element);
+    window.addEventListener("load", init);
+    
+    var sum: number = 0;
+    var div;
+    
+    function init(): void {
+        placeDivs();
+    }
+    
+    function placeDivs(): void {
+        var reis: number = 1;
+        var zeile: number = 0;
+        for (var i: number = 0; i < 64; i++) {
+            var element: HTMLElement = document.createElement("div");
+            if (i < 8) {
+                element.addEventListener("click", selectDiv);
+            }
+            if (((i + zeile) % 2) == 0) {
+                element.className = "board white";
+            }
+            else {
+                element.className = "board black";
+            }
+            if (((i + zeile) % 8) == 0) {
+                zeile++;
+            }
+            element.innerText = " " + rice;
+            rice *= 2;
+            document.body.appendChild(element);
         }
-    });
+        div = document.createElement("div");
+        div.id = "move";
+        document.addEventListener("mousemove", updateReis);
+        document.body.appendChild(div);
+    }
+    
+    function selectDiv(_event: Event): void {
+        var clickedDiv: HTMLDivElement = event.target;
+        if (clickedDiv.classList.toggle("selected")) {
+            sum += parseInt(clickedDiv.textContent);
+        }
+        else {
+            sum -= parseInt(clickedDiv.textContent);
+        }
+    }
+    function updateReis(_event: Event): void {
+        div.style.top = event.clientY + 10 + "px";
+        div.style.left = event.clientX + 10 + "px";
+        div.textContent = "Dezimalsystem: = " + sum + " Hexadezimalsystem: = " + sum.toString(16);
+    }
+    
 }
+
+
