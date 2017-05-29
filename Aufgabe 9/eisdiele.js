6e86e27faabd12c0e645e1d413d79f7ca896b22b
luisefehrenbach <luisefehrenbach@luises-mbp.hfu-open.fu.hs-furtwangen.de>
2017-05-29 11:47:52 +0200

    Aufgabe 9

--- Aufgabe 9/eisdiele.js
+++ Aufgabe 9/eisdiele.js
@@ -6,4 +6,11 @@ Datum: 23.5.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
+var eisdiele;
+(function (eisdiele) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        let fieldsets = document.getElementsByTagName("fieldset");
+    }
+})(eisdiele || (eisdiele = {}));
 //# sourceMappingURL=eisdiele.js.map
--- Aufgabe 9/eisdiele.ts
+++ Aufgabe 9/eisdiele.ts
@@ -6,3 +6,13 @@ Datum: 23.5.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
+
+namespace eisdiele {
+    window.addEventListener("load", init);
+    
+    function init(_event: Event): void {
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+        
+        for 
+    }
+}
