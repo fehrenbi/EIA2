2308f2fc9d982a83245c707827f36fd0dd0471ba
luisefehrenbach <luisefehrenbach@luises-mbp.fritz.box>
2017-07-05 17:20:15 +0200

    Hintergrund und Spinne erstellen

--- Abschlussarbeit/Abschlussarbeit.js
+++ Abschlussarbeit/Abschlussarbeit.js
@@ -9,17 +9,44 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 */
 var abschlussarbeit;
 (function (abschlussarbeit) {
+    window.addEventListener("load", init);
     function init(_event) {
         let canvas;
         canvas = document.getElementsByTagName("canvas")[0];
         abschlussarbeit.crc2 = canvas.getContext("2d");
         drawGround();
+        drawSpider(200, 200);
     }
     // alle Funktionen für Canvas-Elemente
     function drawGround() {
         abschlussarbeit.crc2.beginPath();
         abschlussarbeit.crc2.fillStyle = "#CDAA7D";
-        abschlussarbeit.crc2.rect(0, 0, 1150, 700);
+        abschlussarbeit.crc2.rect(0, 0, 1200, 600);
+        abschlussarbeit.crc2.fill();
+        abschlussarbeit.crc2.closePath();
+    }
+    function drawSpider(_x, _y) {
+        abschlussarbeit.crc2.beginPath();
+        abschlussarbeit.crc2.fillStyle = "#000000";
+        abschlussarbeit.crc2.arc(_x + 2, _y - 2, 5, 0, 2 * Math.PI);
+        abschlussarbeit.crc2.fill();
+        abschlussarbeit.crc2.closePath();
+        abschlussarbeit.crc2.beginPath();
+        abschlussarbeit.crc2.strokeStyle = "#000000";
+        abschlussarbeit.crc2.moveTo(_x, _y);
+        abschlussarbeit.crc2.lineTo(_x - 10, _y - 5);
+        abschlussarbeit.crc2.moveTo(_x, _y);
+        abschlussarbeit.crc2.lineTo(_x - 10, _y + 5);
+        abschlussarbeit.crc2.moveTo(_x, _y);
+        abschlussarbeit.crc2.lineTo(_x + 14, _y - 5);
+        abschlussarbeit.crc2.moveTo(_x, _y);
+        abschlussarbeit.crc2.lineTo(_x + 14, _y + 5);
+        abschlussarbeit.crc2.closePath();
+        abschlussarbeit.crc2.stroke();
+        abschlussarbeit.crc2.beginPath();
+        abschlussarbeit.crc2.fillStyle = "#FFFFFF";
+        abschlussarbeit.crc2.arc(_x, _y - 3, 1, 0, 2 * Math.PI);
+        abschlussarbeit.crc2.arc(_x + 4, _y - 3, 1, 0, 2 * Math.PI);
         abschlussarbeit.crc2.fill();
         abschlussarbeit.crc2.closePath();
     }
--- Abschlussarbeit/Abschlussarbeit.ts
+++ Abschlussarbeit/Abschlussarbeit.ts
@@ -10,7 +10,10 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 
 namespace abschlussarbeit {
     
+    window.addEventListener("load", init);
+    
     export let crc2: CanvasRenderingContext2D;
+    
     
     function init (_event: Event): void {
         let canvas: HTMLCanvasElement;
@@ -18,6 +21,7 @@ namespace abschlussarbeit {
         crc2 = canvas.getContext("2d");
         
         drawGround();
+        drawSpider(200, 200);
     }
     
     
@@ -26,8 +30,35 @@ namespace abschlussarbeit {
     function drawGround(): void {
         crc2.beginPath();
         crc2.fillStyle = "#CDAA7D";
-        crc2.rect(0, 0, 1150, 700);
+        crc2.rect(0, 0, 1200, 600);
         crc2.fill();
         crc2.closePath();
     }
+    
+    function drawSpider(_x: number, _y: number): void {
+        crc2.beginPath();
+        crc2.fillStyle = "#000000";
+        crc2.arc(_x + 2, _y - 2, 5, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.closePath();
+        crc2.beginPath();
+        crc2.strokeStyle = "#000000";
+        crc2.moveTo(_x, _y);
+        crc2.lineTo(_x - 10, _y - 5);
+        crc2.moveTo(_x, _y);
+        crc2.lineTo(_x - 10, _y + 5);
+        crc2.moveTo(_x, _y);
+        crc2.lineTo(_x + 14, _y - 5);
+        crc2.moveTo(_x, _y);
+        crc2.lineTo(_x + 14, _y + 5);
+        crc2.closePath();
+        crc2.stroke();
+        crc2.beginPath();
+        crc2.fillStyle = "#FFFFFF";
+        crc2.arc(_x, _y - 3, 1, 0, 2 * Math.PI);
+        crc2.arc(_x + 4, _y - 3, 1, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.closePath();
+    }
+    
 }
