a2c70e884f100acff37d7a3803dc338ed5641171
luisefehrenbach <luisefehrenbach@luises-mbp.fritz.box>
2017-07-05 16:28:14 +0200

    Dokumente erstellen Abschlussarbeit

---
+++ Abschlussarbeit/Abschlussarbeit.js
@@ -0,0 +1,10 @@
+/*
+Abschlussarbeit:
+Objektorientierte Programmierung: Snake
+Name: Luise Fehrenbach
+Matrikel: 254668
+Datum: 5.7.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+//# sourceMappingURL=Abschlussarbeit.js.map
---
+++ Abschlussarbeit/Abschlussarbeit.ts
@@ -0,0 +1,13 @@
+/*
+Abschlussarbeit:
+Objektorientierte Programmierung: Snake
+Name: Luise Fehrenbach
+Matrikel: 254668
+Datum: 5.7.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+namespace abschlussarbeit {
+    
+}
