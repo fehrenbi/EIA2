4cbe169bf64852aa12f4a8c529623d67f7a10269
luisefehrenbach <luisefehrenbach@luises-mbp.fritz.box>
2017-07-05 16:44:40 +0200

    erste Basics

--- Abschlussarbeit/Abschlussarbeit.js
+++ Abschlussarbeit/Abschlussarbeit.js
@@ -7,4 +7,21 @@ Datum: 5.7.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
+var abschlussarbeit;
+(function (abschlussarbeit) {
+    function init(_event) {
+        let canvas;
+        canvas = document.getElementsByTagName("canvas")[0];
+        abschlussarbeit.crc2 = canvas.getContext("2d");
+        drawGround();
+    }
+    // alle Funktionen für Canvas-Elemente
+    function drawGround() {
+        abschlussarbeit.crc2.beginPath();
+        abschlussarbeit.crc2.fillStyle = "#CDAA7D";
+        abschlussarbeit.crc2.rect(0, 0, 1150, 700);
+        abschlussarbeit.crc2.fill();
+        abschlussarbeit.crc2.closePath();
+    }
+})(abschlussarbeit || (abschlussarbeit = {}));
 //# sourceMappingURL=Abschlussarbeit.js.map
--- Abschlussarbeit/Abschlussarbeit.ts
+++ Abschlussarbeit/Abschlussarbeit.ts
@@ -10,4 +10,24 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 
 namespace abschlussarbeit {
     
+    export let crc2: CanvasRenderingContext2D;
+    
+    function init (_event: Event): void {
+        let canvas: HTMLCanvasElement;
+        canvas = document.getElementsByTagName("canvas")[0];
+        crc2 = canvas.getContext("2d");
+        
+        drawGround();
+    }
+    
+    
+// alle Funktionen für Canvas-Elemente
+    
+    function drawGround(): void {
+        crc2.beginPath();
+        crc2.fillStyle = "#CDAA7D";
+        crc2.rect(0, 0, 1150, 700);
+        crc2.fill();
+        crc2.closePath();
+    }
 }
